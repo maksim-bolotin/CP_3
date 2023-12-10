cb98e0bd86c0ab27456bb17e5d022c9b6ad8fcf3
Maksim Bolotin <vk07122012@gmail.com>
2023-12-11 01:30:50 +0300

    add comments in main.py. update requirements.txt

--- main.py
+++ main.py
@@ -6,9 +6,12 @@ def main():
     data = reading_from_file()
     # создание объектов класса из каждой операции.
     list_of_data = get_operations_data(data)
+    # сортировка объектов класса по дате выполнения операции.
     last_dates = sort_date(list_of_data)
+    # реализация вывода последних пяти выполненных (EXECUTED) операций.
     for i in last_dates[-5:]:
         print(i)
+
 
 if __name__ == '__main__':
     main()
